604cc7c3613c8972fd394ec73f49db47014416a0
David Shi <DavidSnooker@gmail.com>
2025-06-08 02:03:51 -0700

    Rename photos taken by Dazz

--- rename-photos.py
+++ rename-photos.py
@@ -25,17 +25,32 @@ def rename_file(filepath):
     date_taken = get_date_taken(filepath)
     if date_taken:
         old_base, old_ext = os.path.splitext(filepath)
+        ## rename if %Y-%m-%d is already in the filename like taken by dazz
+        dazz_date = f"{date_taken.strftime('%Y-%m-%d')}"
+        if dazz_date in old_base:
+            rename_dazz_file(filepath, dazz_date)
+            return
         # standardize jpg extensions
         if old_ext.lower() == ".jpeg":
             old_ext = ".jpg"
         old_filename = old_base + old_ext.lower()
         new_filename = f"{date_taken.strftime('%Y%m%d')}-{os.path.basename(old_filename)}"
         new_path = os.path.join(os.path.dirname(filepath), new_filename)
-        #print(f"{filepath} -> {new_path}")
+        # print(f"{filepath} -> {new_path}")
         os.rename(filepath, new_path)
 
     else:
         print(f"[{filepath}] missing date info from EXIF")
+
+"""rename photos take by dazz"""
+def rename_dazz_file(filepath, date):
+    old_base, old_ext = os.path.splitext(filepath)
+    new_date = date.replace('-', '')
+    old_filename = old_base + old_ext.lower()
+    new_filename = new_date + '-' + '-'.join([s for s in os.path.basename(old_filename).split() if s != date])
+    new_path = os.path.join(os.path.dirname(filepath), new_filename)
+    # print(f"{filepath} -> {new_path}")
+    os.rename(filepath, new_path)
 
 def traverse_files(directory):
     for root, directories, files in os.walk(directory):
